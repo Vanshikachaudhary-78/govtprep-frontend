9f5e4c60d890524c66e18ad5b1a3634bde866ad7
Vanshikachaudhary-78 <chaudharyvanshika58@gmail.com>
2026-06-29 14:57:05 +0530

    '

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import Signup from "./pages/signup"
 import News from "./pages/News"
 import Saved from "./pages/saved"
 
